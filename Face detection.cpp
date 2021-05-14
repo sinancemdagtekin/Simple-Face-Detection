b2c4a863f9e56964a194b531b7c939f5318e16e0
Sinan Cem Dağtekin <sinancemdagtekin@gmail.com>
2021-05-14 16:20:26 +0300

    Face detection using C++& opencv
    Here I made a simple face detection software inside the main function, using Haard Cascade for frontal face with embedded opencv Cascade Classifier.
    To detect faces, I simply used the good old Viola-Jones method.

---
+++ Face detection.cpp
@@ -0,0 +1,44 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+
+	std::string path = "Resources/video.mp4";
+	cv::VideoCapture cap(path);
+
+	cv::Mat img;
+
+	//load the Haar Cascade
+	CascadeClassifier faceCascade;
+	faceCascade.load("Resources/haarcascade_frontalface_default.xml");
+
+	//to check if it is loaded properly
+	if (faceCascade.empty()) { cout << "xml file not loaded" << endl; }
+
+	while (true) {
+
+		cap.read(img);
+
+		//detect the faces and store them
+		vector<Rect> faces;
+		faceCascade.detectMultiScale(img, faces, 1.1, 10);
+
+		//iterate the faces which detected
+
+		for (int i = 0; i < faces.size(); i++) {
+
+			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 2);
+		}
+
+		cv::imshow("Image", img);
+
+		cv::waitKey(10);//to wait 10 milisecond for each image
+
+	}
+}
